a906d9fd2b2aa7277b769b1d194a54535f948f71
heshandevaka <heshan.devaka@gmail.com>
2024-10-20 17:15:57 +0000

    Init commit

--- examples/scripts/run_MMLU_benchmark.sh
+++ examples/scripts/run_MMLU_benchmark.sh
@@ -8,13 +8,13 @@ LAMBD_LIST="0.25 0.5 0.75"
 # for seq algorithms
 for algo in $ALGOS_1; do
    for na_or_lambd in $NA_LIST; do
-        python3 xright/MMLU_deep_eval_llama3.py --algo $algo --na_or_lambd $na_or_lambd
+        python3 examples/MMLU_deep_eval_llama3.py --algo $algo --na_or_lambd $na_or_lambd
     done
  done
 
  # for moo algorithms
 for algo in $ALGOS_2; do
    for na_or_lambd in $LAMBD_LIST; do
-        python3 xright/MMLU_deep_eval_llama3.py --algo $algo --na_or_lambd $na_or_lambd
+        python3 examples/MMLU_deep_eval_llama3.py --algo $algo --na_or_lambd $na_or_lambd
     done
  done
